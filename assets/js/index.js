758cc98c7e0c2a9d33f396449e93c4a1643f4c6b
Marco Verrone <programma.meiv@gmail.com>
2024-06-07 19:18:47 +0200

    Error-box style + aniamtion + testBtn

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,14 @@
 const weatherBox = document.querySelector(".weather-box")
+const errorBox = document.querySelector(".error-box")
 const searchBtn = document.querySelector(".search-btn")
 const main = document.querySelector("main")
 
 // searchBtn.addEventListener("click", function(){
 //   weatherBox.style.display = "flex"
 //   main.style.height = "520px"
+// })
+
+// searchBtn.addEventListener("click", function(){
+//   errorBox.style.display = "flex"
+//   main.style.height = "320px"
 // })
