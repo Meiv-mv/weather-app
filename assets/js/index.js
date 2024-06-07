b1fc7d74e447b1dda7b8bcfafac6d158f182983b
Marco Verrone <programma.meiv@gmail.com>
2024-06-07 17:11:48 +0200

    weather-box style and animation +  test button

---
+++ assets/js/index.js
@@ -0,0 +1,8 @@
+const weatherBox = document.querySelector(".weather-box")
+const searchBtn = document.querySelector(".search-btn")
+const main = document.querySelector("main")
+
+// searchBtn.addEventListener("click", function(){
+//   weatherBox.style.display = "flex"
+//   main.style.height = "520px"
+// })
